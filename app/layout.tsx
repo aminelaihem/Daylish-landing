c6c424957f635c9220e96639c42e3fbdd6d37592
aminelhm <131161302+aminelhm@users.noreply.github.com>
2025-05-04 04:08:03 +0200

    ajout du fav icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type React from "react"
 import "./globals.css"
 import type { Metadata } from "next"
 import { Playfair_Display } from "next/font/google"
+import Head from "next/head"
 
 const playfair = Playfair_Display({
   subsets: ["latin"],
@@ -9,8 +10,10 @@ const playfair = Playfair_Display({
 })
 
 export const metadata: Metadata = {
-  title: "Daylish - Découvrez des plats faits maison",
-  description: "L'application qui vous fait voyager tout en restant chez vous"
+  title: "Daylish - Landing Page",
+  icons: {
+    icon: "/favicon.svg",
+  },
 }
 
 export default function RootLayout({
